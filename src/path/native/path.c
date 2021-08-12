94d424b596756a7aa7806a6a5436c2120915cce3
Evan Ovadia <verdagon@gmail.com>
2021-08-11 19:08:43 -0700

    Added tests for path

--- src/path/native/path.c
+++ src/path/native/path.c
@@ -26,7 +26,7 @@
 
 static int8_t is_file_internal(char* path) {
 #ifdef _WIN32
-  return !!(GetFileAttributes(path) & FILE_ATTRIBUTE_DIRECTORY);
+  return (GetFileAttributes(path) & FILE_ATTRIBUTE_DIRECTORY) == 0;
 #else
   struct stat path_stat;
   stat(path, &path_stat);
